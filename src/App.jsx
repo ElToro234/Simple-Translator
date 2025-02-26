4272fd1185fc3534d17d1f80ef6c617630300113
Alexander Liu <alexanderliuxinlu@gmail.com>
2025-02-26 12:33:08 -0500

    start

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Translator</h1>
+
+      <form>
+        <div className="choices">
+          <input type="radio" id="english" name="language"/>
+          <label htmlFor="english">EN</label>
+
+          <input type="radio" id="spanish" name="language"/>
+          <label htmlFor="spanish">SP</label>
+
+          <input type="radio" id="french" name="language"/>
+          <label htmlFor="french">FR</label>
+        </div>
+
+        <textarea 
+          name="message"
+          placeholder="Type your message here.."
+         ></textarea>
+
+         <button type="submit"> Translate</button>
+      </form>
+
+      <div className="translation">
+        <div className="copy-btn">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184" />
+        </svg>
+
+        </div>
+
+        Translate Text
+        </div>
+
+        <div className="notification">
+          Copied to clipboard
+          </div>
+    </div>
+  )
+}
+
+export default App
+
